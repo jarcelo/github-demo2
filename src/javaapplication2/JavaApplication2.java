c9e8dab5fce0fac08358a43a083aa1d83a248de8
BA201 <BA201@MCBA201-C05.classrooms.stlcc>
2016-10-19 11:31:00 -0500

    Add hello github

--- src/javaapplication2/JavaApplication2.java
+++ src/javaapplication2/JavaApplication2.java
@@ -16,5 +16,6 @@ public class JavaApplication2 {
      */
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+        System.out.println("Hello, GitHub!");
     }
 }
